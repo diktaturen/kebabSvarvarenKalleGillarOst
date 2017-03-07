7a6d824c46bf1250411761f4875c8dc643b0fbdd
Victor Limé <victorlime97@gmail.com>
2017-03-07 23:21:13 +0100

    Kyptering klass.
    Bara att anropa men Encryption enc = new Encryption()
    enc.Kryptering()

---
+++ miniforensikprojekt/src/Encryption.java
@@ -0,0 +1,67 @@
+﻿
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Created by limE on 2017-03-07.
+ */
+public class Encryption
+{
+    public void Kryptering(){
+        //variabler
+        String text, path, spara, klar = "";;
+
+        //scanner
+        Scanner in = new Scanner(System.in);
+
+        //fråga användaren vilken fil
+        System.out.println("Mata in Abs-path inkl filsorten");
+        path = in.nextLine();
+
+        System.out.println("Vart vill du spara den kryptreade filen, hela namned inkl filsort");
+        spara = in.nextLine();
+
+        File file = new File(path);
+        File create = new File(spara);
+        create.getParentFile().mkdirs();
+
+        try{
+            Scanner filescan = new Scanner(file);
+
+            while(filescan.hasNext()) {
+
+                text = filescan.nextLine();
+
+                //anropa metod jävel
+                String done = encrypt(text, klar);
+
+                try {
+                    PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(create, true)));
+
+                    writer.println(done);
+                    writer.flush();
+                    writer.close();
+
+                } catch (IOException e) {
+
+                    System.out.println(e);
+                }
+
+            }
+
+        } catch(Exception e) {System.out.println(e); }
+
+        System.out.println("Klar!");
+    }
+    public String encrypt(String text, String klar_text) {
+
+        for(int i = 0; i < text.length(); i ++){
+
+            int ascii_ny = (int)text.charAt( i ) + 1;
+
+            klar_text = klar_text + ((char)ascii_ny);
+        }
+        return klar_text;
+    }
+}
+
