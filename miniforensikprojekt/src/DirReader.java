920f64e01e5ab549e1c200de86720de122345938
Martin H <cmartinhoffman@gmail.com>
2017-03-11 18:59:42 +0100

    kommenterat ut koden bättre

--- miniforensikprojekt/src/DirReader.java
+++ miniforensikprojekt/src/DirReader.java
@@ -5,30 +5,30 @@ import java.io.File;
 /**
  * Created by poff on 2017-02-28.
  */
+ 
 public class DirReader {
     public static void main(String[] args) {
 
-        FileWalker fw = new FileWalker();
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); 
 
-        System.out.println("Mata in abs-Path");
-        String inPath = input.next();
-        walk(inPath);
+        System.out.println("Mata in abs-Path"); //be om indata
+        String inPath = input.next(); //hämta indata
+        walk(inPath); //påkalla walk-metoden som vandrar genom directoryn man valt
 
     }
 
    public static void walk(String path) {
 
-        File startPath = new File(path);
-        File[] list = startPath.listFiles();
+        File startPath = new File(path); 
+        File[] list = startPath.listFiles(); //skapar en File-array utav alla items i directoryn man matat in
 
-        if (list == null) return;
+        if (list == null) return; //om listan är tom händer ingenting.
 
-        for (File f : list) {
-            if (f.isDirectory()) {
-                walk(f.getAbsolutePath());
-                System.out.println("Dir:" + f.getAbsoluteFile());
-            } else {
+        for (File f : list) { //start av for-loop som kollar varje item i arrayen
+            if (f.isDirectory()) { //if-sats om itemet är ett directory 
+                walk(f.getAbsolutePath()); //så påkallas walk-metoden på directoryt
+                System.out.println("Dir:" + f.getAbsoluteFile()); //printar absolutpathen för directoryn
+            } else { //om itemet inte är ett directory så är det en fil och printar då absolut-pathen till filen
                 System.out.println("File:" + f.getAbsoluteFile());
             }
         }
